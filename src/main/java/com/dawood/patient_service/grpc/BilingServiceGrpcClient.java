bb88cc896ab301a039dda2c101e71ceda11dac0d
dawood <sulaimondawod@gmail.com>
2025-04-18 11:05:03 +0100

    working on gprc client

---
+++ src/main/java/com/dawood/patient_service/grpc/BilingServiceGrpcClient.java
@@ -0,0 +1,33 @@
+package com.dawood.patient_service.grpc;
+
+import billingService.BillingRespnse;
+import billingService.BillingServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BillingServiceGrpcClient {
+    private static final Logger log = LoggerFactory.getLogger(BillingServiceGrpcClient.class);
+    private final BillingServiceGrpc.BillingServiceBlockingStub billingServiceBlockingStub;
+
+    public BillingServiceGrpcClient(
+            @Value("${billing.service.address:localhost}") String billingAddress,
+            @Value("${billing.service.grpc.port:9001}") int billingServerPort
+    ){
+    log.info("Connecting to Billing Service Service at {}, {}", billingAddress, billingServerPort);
+
+        ManagedChannel channel = ManagedChannelBuilder.forAddress(billingAddress, billingServerPort)
+                .usePlaintext().build();
+        billingServiceBlockingStub = BillingServiceGrpc.newBlockingStub(channel);
+    }
+
+    public BillingRespnse createBillingAccount (String name, String patientId, String email ){
+
+    }
+
+
+}
